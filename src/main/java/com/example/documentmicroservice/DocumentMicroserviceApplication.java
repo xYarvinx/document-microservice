cc0aa6411551d607577e80d1608aaad66fd2cbb9
Yaroslav <otebator@gmail.com>
2024-09-20 11:04:15 +1000

    init commit

---
+++ src/main/java/com/example/documentmicroservice/DocumentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.documentmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/documentmicroservice/DocumentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
